e5221e9d8888bbe931c299edfde2c84518951b96
TylerKneffler <TKneffler@gmail.com>
2024-02-28 19:03:15 -0500

    update

--- chunkMiner.lua
+++ chunkMiner.lua
@@ -2,11 +2,48 @@ args = {...}
 
 local home = vector.new(1,1,1)
 local digto = vector.new(args[1],args[2],args[3])
+local current = vector.new(1,1,1)
 
 print(home)
 print(digto)
 
 local turn = true
+
+function ivFull()
+    for i = 1,16 do
+        if turtle.getItemSpace(i) > 0 then
+        return false
+        end
+    end
+    return true
+end
+
+function updatePos(x,y,z)
+    current.x = x
+    current.y = y
+    current.z = z
+    print(x," ",y," ",z)
+end
+
+
+function resupply(x,y,z)
+    for y = current.y, home.y, 1 do
+        turtle.up()
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i = 2,16 do
+        turtle.select(i)
+        turtle.drop(64)
+    end
+    
+    turtle.turnRight()
+    turtle.turnRight()
+    
+    for y = current.y, home.y, 1 do
+        turtle.down()
+    end
+end
 
 for y = home.y, digto.y, -1 do
     for x = home.x, digto.x, 1 do
@@ -14,7 +51,7 @@ for y = home.y, digto.y, -1 do
             if z < digto.z then
                 turtle.dig()
                 turtle.forward()
-                print(x," ",y," ",z)
+                updatePos(x,y,z)
             end
         end
         print(x," ~= ",digto.x)
